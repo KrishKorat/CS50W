7ed69089eea3f827c8c8dc91b959e773b429ee32
CelestialMist7 <krishkorat33@gmail.com>
2025-05-26 16:47:27 +0530

    flight.py created

---
+++ Lecture_3/flight.py
@@ -0,0 +1,27 @@
+class Flight():
+
+  def __init__(self, capacity):
+    self.capacity = capacity
+    self.passangers = []
+  
+  def add_passangers(self, name):
+    if not self.open_seats():
+      return False
+    self.passangers.append(name)
+    return True
+
+  def open_seats(self):
+    return self.capacity - len(self.passangers)
+  
+
+
+
+flight = Flight(3)
+
+people = ["Harry", "Ron", "Hermione", "Ginny"]
+
+for person in people:
+  if flight.add_passangers(person):
+    print(f"Added {person} to flight successfully")
+  else:
+    print(f"No seats availabe for {person}")
