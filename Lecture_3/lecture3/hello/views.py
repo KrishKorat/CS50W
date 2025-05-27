274d96a003d547b81bd926eff162f3c9abaffc06
CelestialMist7 <krishkorat33@gmail.com>
2025-05-27 08:11:21 +0530

    Lecture_3 routes performed

--- Lecture_3/lecture3/hello/views.py
+++ Lecture_3/lecture3/hello/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+  return render(request, 'hello/index.html')
+
+def krish(request):
+  return HttpResponse("Hello, krish!")
+
+
+def greet(request, name):
+  return HttpResponse(f"Hello, {name.capitalize()}!")
