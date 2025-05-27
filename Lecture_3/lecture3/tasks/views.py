7c9a30eb7c25171d7066eb280b6c438f5f2b4a4d
CelestialMist7 <krishkorat33@gmail.com>
2025-05-27 13:44:21 +0530

    Lecture_3 tasks created

---
+++ Lecture_3/lecture3/tasks/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+tasks = ['foo', 'bar', 'buz']
+
+# Create your views here.
+def index(request):
+  return render(request, "tasks/index.html", {
+    "tasks": tasks
+  })
