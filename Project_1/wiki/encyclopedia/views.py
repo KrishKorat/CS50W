4d0b2ceb230b05ea3710e02290f3b94013258fea
CelestialMist7 <krishkorat33@gmail.com>
2025-05-30 09:22:34 +0530

    Project_1 wiki entry funtion added

--- Project_1/wiki/encyclopedia/views.py
+++ Project_1/wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import markdown
 
 from . import util
 
@@ -7,4 +8,19 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def entry(request, title):
+    content = util.get_entry(title)
+
+    if content is None:
+        return render(request, "encyclopedia/error.html", {
+            "title": "Error",
+            "message": "Entry not found."
+        })
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": markdown(content)
+    })
+
 
