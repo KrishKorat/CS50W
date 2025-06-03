b444e434c4a73c3142977bcb4e9aca59815c6db7
CelestialMist7 <krishkorat33@gmail.com>
2025-06-03 07:04:04 +0530

    Lecture_4 airline migration performed

--- Lecture_4/airline/flights/models.py
+++ Lecture_4/airline/flights/models.py
@@ -1,7 +1,81 @@
 from django.db import models
 
 # Create your models here.
+
+class Airport(models.Model):
+  code = models.CharField(max_length=3)
+  city = models.CharField(max_length=64)
+
+  def __str__(self):
+    return f"{self.city} ({self.code})" 
+
 class Flight(models.Model):
-  origin = models.CharField(max_length=64)
-  destination = models.CharField(max_length=64)
+  origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+  destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
   duration = models.IntegerField()
+
+  def __str__(self):
+    return f"{self.id}: {self.origin} to {self.destination}"
+
+
+'''
+>>> from flights.models import Flight
+>>> f = Flight(origin="New York", destination="London", duration=415)
+>>> f.save()
+>>> Flight.objects.all()
+<QuerySet [<Flight: Flight object (1)>]>
+
+
+-------------------------------------------- After defining __str__
+
+>>> from flights.models import Flight
+>>> flights = Flight.objects.all()
+>>> flights
+<QuerySet [<Flight: 1: New York to London>]>
+
+>>> flight = flights.first()
+>>> flight 
+<Flight: 1: New York to London>
+>>> flight.id
+1
+>>> flight.origin 
+'New York'
+>>> flight.destination
+'London'
+'''
+
+
+
+
+'''
+-------------------------------------------------- After changing to foreign keys
+
+>>> from flights.models import Airport
+
+>>> jfk = Airport(code="JFK", city="New York")
+>>> jfk.save()
+
+>>> lhr = Airport(code="LHR", city="London")
+>>> lhr.save()
+
+>>> cdg = Airport(code="CDG", city="Paris")
+>>> cdg.save()
+
+>>> nrt = Airport(code="NRT", city="Tokyo")
+>>> nrt.save()
+
+>>> f = Flight(origin=jfk, destination=lhr, duration=415)
+>>> f.save()
+>>> f
+<Flight: 1: New York (JFK) to London (LHR)>
+
+>>> f.origin  
+<Airport: New York (JFK)>
+>>> f.origin.city
+'New York'
+>>> f.origin.code
+'JFK'
+
+>>> lhr.arrivals.all()
+<QuerySet [<Flight: 1: New York (JFK) to London (LHR)>]>
+'''
