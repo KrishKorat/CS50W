0b1785128591ba5c5e65a006572fc5ff41386c94
CelestialMist7 <krishkorat33@gmail.com>
2025-06-06 18:49:06 +0530

    Lecture_4 airline feature added to add passengers in web page

--- Lecture_4/airline/flights/models.py
+++ Lecture_4/airline/flights/models.py
@@ -19,7 +19,7 @@ class Flight(models.Model):
 
 
 
-class Passanger(models.Model):
+class Passenger(models.Model):
   first = models.CharField(max_length=64)
   last = models.CharField(max_length=64)
   flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
--- Lecture_4/airline/flights/views.py
+++ Lecture_4/airline/flights/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponsePermanentRedirect
+from django.urls import reverse
 
-from .models import Flight
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
@@ -11,7 +13,18 @@ def index(request):
 
 def flight(request, flight_id):
   flight = Flight.objects.get(pk=flight_id)
+  non_passengers = Passenger.objects.exclude(flights=flight).all()
   return render(request, "flights/flight.html", {
     "flight": flight,
-    "passengers": flight.passengers.all()
+    "passengers": flight.passengers.all(),
+    "non_passengers": non_passengers
   })
+
+
+def book(request, flight_id):
+  if request.method == "POST":
+    flight = Flight.objects.get(pk=flight_id)
+    passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+    passenger.flights.add(flight)
+
+    return HttpResponsePermanentRedirect(reverse("flight", args=(flight.id, )))
